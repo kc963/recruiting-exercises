4f3f01de8f2c45131353d68186e51ae718ca388e
Kapil Chopra <kapilchopra963@gmail.com>
2019-06-15 18:49:39 -0400

    Added source code

---
+++ inventory-allocator/src/InventoryAllocator.java
@@ -0,0 +1,77 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public final class InventoryAllocator {
+
+	public List<Map<String, Map<String, Integer>>> getCheapestShipment(Order order, List<Warehouse> warehouses){
+		
+		List<Map<String, Map<String, Integer>>> orderShipment = new LinkedList<Map<String, Map<String, Integer>>>();
+		
+		if(order == null) {
+			System.out.println("ERROR: No order provided");
+			return orderShipment;
+		}
+		if(warehouses == null) {
+			System.out.println("ERROR: No warehouses provided");
+			return orderShipment;
+		}
+		
+		Map<String, Integer> orderedItems = new HashMap<String, Integer>();
+		orderedItems.putAll(order.getOrderedItems());
+		
+		if(orderedItems.isEmpty()) {
+			System.out.println("ERROR: No items ordered");
+		}else if(warehouses.isEmpty()){
+			System.out.println("ERROR: No warehouses selected");
+		}else {
+			
+			for(Warehouse warehouse : warehouses) {
+				Map<String, Integer> warehouseInventory = warehouse.getInventory();
+				
+				Map<String, Integer> itemsSupplied = new HashMap<String, Integer>();
+				Map<String, Map<String, Integer>> shipment = new HashMap<String, Map<String, Integer>>();
+				
+				for(Map.Entry<String, Integer> item : orderedItems.entrySet()) {
+					String orderedItemName = item.getKey();
+					int orderedItemValue = item.getValue();
+					
+					if(warehouseInventory.containsKey(orderedItemName)) {
+						if(warehouseInventory.get(orderedItemName) < orderedItemValue) {
+							itemsSupplied.put(orderedItemName, warehouseInventory.get(orderedItemName));
+						}
+						else {
+							itemsSupplied.put(orderedItemName, orderedItemValue);
+						}
+					}
+				}
+				
+				//Update order
+				for(Map.Entry<String, Integer> item : itemsSupplied.entrySet()) {
+					if(orderedItems.get(item.getKey()) > item.getValue()) {
+						orderedItems.replace(item.getKey(), orderedItems.get(item.getKey()) - item.getValue());
+					}else {
+						orderedItems.remove(item.getKey());
+					}
+				}
+				
+				//Ship the items
+				if(!itemsSupplied.isEmpty()) {
+					shipment.put(warehouse.getName(), itemsSupplied);
+					orderShipment.add(shipment);
+				}
+				
+			}
+			
+			if(!orderedItems.isEmpty()) {
+				System.out.println("ERROR: Out of Stock");
+				orderShipment.clear();
+			}
+			
+		}
+		
+		return orderShipment;
+	}
+	
+}
---
+++ inventory-allocator/src/Warehouse.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Warehouse {
+
+	private String name;
+	private Map<String, Integer> inventory;
+	
+	public Warehouse() {
+		name = "";
+		inventory = new HashMap<String, Integer>();
+	}
+	
+	public Warehouse(String warehouseName, Map<String, Integer> inventory) {
+		name = warehouseName;
+		this.inventory = inventory;
+	}
+
+	public Warehouse(Warehouse warehouse) {
+		name = warehouse.name;
+		inventory = warehouse.inventory;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String warehouseName) {
+		name = warehouseName;
+	}
+	
+	public Map<String, Integer> getInventory() {
+		return inventory;
+	}
+	
+	public void setInventory(Map<String, Integer> inventory) {
+		this.inventory = inventory;
+	}
+	
+	public String toString() {
+		return "{ name : " + name + ", inventory : " + inventory + " }";
+	}
+	
+}
+
+//	Warehouse
+//		name
+//		inventory
+//
+//
+//
+//
